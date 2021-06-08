f24363b0ce9542f214e96adc3edd50e888b73cc2
basakstuff <basaker98@gmail.com>
2021-06-08 05:36:07 +0300

    stripe issue

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -7,29 +7,19 @@ const factory = require('./handlerFactory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
-  
   const tour = await Tour.findById(req.params.tourId);
-  console.log(tour);
+  // console.log(tour);
 
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-
-   //  success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
-   //    req.params.tourId
-   //  }&user=${req.user.id}&price=${tour.price}`,
-
     success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
-
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
-
     customer_email: req.user.email,
-
     client_reference_id: req.params.tourId,
-
     line_items: [
       {
-        name: `${(tour.name).toLowerCase().replace(/ /g, "-")} Tour`,
+        name: `${tour.name} Tour`,
         description: tour.summary,
         images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
         amount: tour.price * 100,
@@ -58,12 +48,17 @@ exports.webhookCheckout = (req, res, next) => {
 
   let event;
   try {
-    event = stripe.webhooks.constructEvent(req.body, signature, process.env.STRIPE_WEBHOOK_SECRET);
+    event = stripe.webhooks.constructEvent(
+      req.body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
   } catch (err) {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
 
-  if (event.type === 'checkout.session.completed') createBookingCheckout(event.data.object);
+  if (event.type === 'checkout.session.completed')
+    createBookingCheckout(event.data.object);
 
   res.status(200).json({ received: true });
 };
